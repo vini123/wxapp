8c0286ba287eb7802d71939b25262df99a230ab4
zhoulin <vini123@sina.cn>
2019-06-06 15:25:01 +0800

    订单和消息title修改

--- pages/demand/index.vue
+++ pages/demand/index.vue
@@ -1,11 +1,41 @@
 <template>
-	<view>
-		
+	<view class="container" :class="transRole">
+		<view class="nav-bar" :style="{ height: statusBarHeight + 44 + 'px', 'padding-top': statusBarHeight + 'px' }">
+			<view class="content">
+				<text class="title text-size-10 text-color-2">{{ title }}</text>
+			</view>
+		</view>
+
+		<scroll-view scroll-y class="frame" :style="{ 'margin-top': statusBarHeight + 44 + 'px' }"></scroll-view>
 	</view>
 </template>
 
 <script>
+import { mapState } from 'vuex';
+
+export default {
+	data() {
+		return {
+			title: '',
+			statusBarHeight: this.statusBarHeight,
+			sq: 'role-1'
+		};
+	},
+	onLoad() {
+		if (this.role == 'role-3') {
+			this.title = '订单';
+		} else {
+			this.title = '抢单';
+		}
+	},
+	computed: {
+		...mapState(['role']),
+		transRole() {
+			return this.role;
+		}
+	},
+	methods: {}
+};
 </script>
 
-<style>
-</style>
+<style lang="scss"></style>
--- pages/message/index.vue
+++ pages/message/index.vue
@@ -1,11 +1,37 @@
 <template>
-	<view>
-		
+	<view class="container" :class="transRole">
+		<view class="nav-bar" :style="{ height: statusBarHeight + 44 + 'px', 'padding-top': statusBarHeight + 'px' }">
+			<view class="content">
+				<text class="title text-size-10 text-color-2">{{ title }}</text>
+			</view>
+		</view>
+
+		<scroll-view scroll-y class="frame" :style="{ 'margin-top': statusBarHeight + 44 + 'px' }"></scroll-view>
 	</view>
 </template>
 
 <script>
+import { mapState } from 'vuex';
+
+export default {
+	data() {
+		return {
+			title: '消息',
+			statusBarHeight: this.statusBarHeight,
+			sq: 'role-1'
+		};
+	},
+	onLoad() {
+
+	},
+	computed: {
+		...mapState(['role']),
+		transRole() {
+			return this.role;
+		}
+	},
+	methods: {}
+};
 </script>
 
-<style>
-</style>
+<style lang="scss"></style>
